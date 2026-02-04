3af9dde06557995f3ee95907969ec25ae9ee4098
likithamedapati <likithamedapati@gmail.com>
2026-02-04 22:49:52 +0530

    Added queue system setup

---
+++ main.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+print("Welcome to Smart Task Queue Scheduler 🧾")
+print("-" * 40)
+
+task_queue = []
+completed_tasks = []
+completed_time = {}
+
+print("System ready. No tasks yet.")
